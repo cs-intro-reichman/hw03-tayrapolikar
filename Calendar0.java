92c371baf7507ea4a052a7d5c2c9ad72ad76587f
tayrapolikar <tayra.polikar@post.idc.ac.il>
2024-01-08 17:53:14 +0200

    test 8

--- Calendar0.java
+++ Calendar0.java
@@ -2,44 +2,47 @@
  * Checks if a given year is a leap year or a common year,
  * and computes the number of days in a given month and a given year. 
  */
-public class Calendar0 {	
-	
-	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
+public class Calendar0 {
 	public static void main(String args[]) {
-		/* int year = Integer.parseInt(args[0]);
+		int year = Integer.parseInt(args[0]);
 		isLeapYearTest(year);
 		nDaysInMonthTest(year);
 	}
-		 
-	// Tests the isLeapYear function.
+
 	private static void isLeapYearTest(int year) {
 		String commonOrLeap = "common";
-		if (isLeapYear(year)) {
+		if(isLeapYear(year)){
 			commonOrLeap = "leap";
 		}
-		System.out.println(year + " is a " + commonOrLeap + " year");  
+		System.out.println(year + " is a " + commonOrLeap + " year");
 	}
 
-	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		int month = 1;
+		int days = nDaysInMonth(month, year);
+		for (int i = 1; i <13 ; i++) {
+			System.out.println("Month " + i + " has " + nDaysInMonth(i,year) + " days  ");
+		}
+
 	}
 
-	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
+		if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+			return true;
+		}
 		return false;
 	}
-	 
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
-	public static int nDaysInMonth(int month, int year) {
-		// Replace the following statement with your code
-		return 0;
 
-		 */
+	public static int nDaysInMonth(int month, int year) {
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			return 31;
+		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			return 30;
+		} else if (month == 2 && year%4!=0) {
+			return 28;
+		} else if (month == 2 && year%4==0) {
+			return 29;
+		}
+		return 0;
 	}
 }
-
